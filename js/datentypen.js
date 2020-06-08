dde2dc69e3de5d60a2d97ac1119c98bef85aec1e
Stefan Schwinghammer <ake_schmidi@me.com>
2020-06-08 07:18:02 +0200

    add some fix return value

--- js/datentypen.js
+++ js/datentypen.js
@@ -1,7 +1,19 @@
-var laenge = 16;
-var vorname = "John"
-var x = {vorname:"John", nachname:"Doe"};
+var laenge = 16; //Number
+var vorname = "John"; //String
+var sauber = true; //Boolian
+var x = {vorname:"John", nachname:"Doe"}; //Object
+
+
+var tuer = {farbe: "gruen", offen: false, geschlossen: true, hoehe:220, breite:100 }
+
+var wandtafel = {farbe: "Rot", sauber:false, dreckig:true, groesse:20 }
+
+
 
 function sagLaenge() {alert(laenge);}
 function sagVorname() {alert(vorname);}
 function sagVorNachname() {alert(x);}
+
+function sagWandtafelFarbe() {alert(wandtafel.farbe);}
+function sagWandtafelSauber() { alert(wandtafel.sauber)}
+function sagWandtafelGroesse() {alert(wandtafel.groesse)}
---
+++ js/gibWasZurueck.js
@@ -0,0 +1,3 @@
+function zurueck(4,3) {
+  return 4;
+}
